af4a5d70d7f6b592a689f787c16314b9c3a87892
Milan Bhandari <milanbhandari2013@gmail.com>
2018-11-26 23:18:05 -0500

    Add files via upload
    Dynamic modal

--- script.js
+++ script.js
@@ -19,3 +19,30 @@
 //     });
 
 // });
+
+jQuery(document).ready(function($) {
+    $(".element").click(function(event) {   //get the position of data
+    var id= $(this).data('pos') -1;  
+    
+    //alert(id);
+    $.getJSON( 'PeriodicTableJSON.json', function(json ) {
+        var items = [];
+        
+        //cleae modal before
+        $(".modal-content").empty();
+        items.push("<span class='close' onclick='closemodal()'>&times;</span>");
+        items.push( "<h3>" + json.elements[id].name + "</h3>" );
+        items.push( "<br/> <b>Atomic Mass:</b> " + json.elements[id].atomic_mass);
+        items.push( "   </t> <b>Boiling Point:</b> " + json.elements[id].boil);
+        items.push( "   </t> <b>Melting Point:</b> " + json.elements[id].melt);
+        items.push( "<br/> <br/> <p>" + json.elements[id].summary+ "</p>" );
+        $(".modal-content").append(items);
+       });
+
+       $("#myModal").css({display: "block"});
+      
+     
+    });   
+    });
+
+   
